713c214d5c5b92762608e9dc43c03b58f717b34c
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-12 15:09:49 +0100

    Update install.php

--- install.php
+++ install.php
@@ -15,8 +15,8 @@ rex_sql_table::get(rex::getTable('erecht24'))
 rex_sql_table::get(rex::getTable('erecht24_texts'))
     ->ensurePrimaryIdColumn()
     ->ensureColumn(new rex_sql_column('domain', 'varchar(191)'))
-    ->ensureIndex(new rex_sql_index('domain', ['domain'], rex_sql_index::UNIQUE))
     ->ensureColumn(new rex_sql_column('type', 'varchar(32)'))  // imprint, privacy_policy, privacy_policy_social_media
+    ->ensureIndex(new rex_sql_index('domain_type', ['domain', 'type'], rex_sql_index::UNIQUE))  // Kombinierter Index
     ->ensureColumn(new rex_sql_column('html_de', 'text'))
     ->ensureColumn(new rex_sql_column('html_en', 'text'))
     ->ensureColumn(new rex_sql_column('last_fetch', 'datetime')) // Neues Feld für letzten Abruf
